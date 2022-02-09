1701d5a2954600a386c72c76ba46c4bc0af3b93f
jean741 <iattiglah@gmail.com>
2022-02-09 20:58:06 +0100

    init ms-order

---
+++ micro_service-order/src/main/java/com/ondev/order/MicroServiceOrderApplication.java
@@ -0,0 +1,13 @@
+package com.ondev.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceOrderApplication.class, args);
+	}
+
+}
---
+++ micro_service-order/src/test/java/com/ondev/order/MicroServiceOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ondev.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiceOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
