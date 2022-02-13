56e99f57a6abb859797b26bcec5abb56833b266d
Mohamed DIABY <mdiaby00@gmail.com>
2022-02-13 10:43:18 +0100

    add openfeign

--- micro_service-order/src/main/java/com/ondev/order/proxy/ProductProxy.java
+++ micro_service-order/src/main/java/com/ondev/order/proxy/ProductProxy.java
@@ -1,13 +1,13 @@
 package com.ondev.order.proxy;
 
 import com.ondev.order.bean.ProductBean;
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
-@FeignClient(name="microservice-product",url = "loclahost:8084")
+@FeignClient(name="microservice-product",url = "localhost:8084")
 public interface ProductProxy {
 
     @PostMapping("/api/product/getAllProductsByIds")
