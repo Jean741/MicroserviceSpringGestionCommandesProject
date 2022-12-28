ec251440a7a3a8b168da20ec50321476b2cd90b0
jean741 <iattiglah@gmail.com>
2022-12-29 00:41:51 +0100

    Add api get way

---
+++ api-getway-gestioncommandes/src/main/java/com/ondev/apigatewaygestioncommandes/ApiGatewayGestioncommandesApplication.java
@@ -0,0 +1,11 @@
+package com.ondev.apigatewaygestioncommandes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+@SpringBootApplication
+public class ApiGatewayGestioncommandesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayGestioncommandesApplication.class, args);
+    }
+}
---
+++ api-getway-gestioncommandes/src/test/java/com/ondev/apigatewaygestioncommandes/ApiGatewayGestioncommandesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ondev.apigatewaygestioncommandes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayGestioncommandesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
